7bb46b4fb6a3bbc16c4a611401361f9fbe21f335
ravibhagw <38326056+ravibhagw@users.noreply.github.com>
2018-05-07 17:40:44 -0400

    working code for getClubStats, getDetailedClubStats, getClubId

--- chelstats.js
+++ chelstats.js
@@ -4,22 +4,49 @@ var https = require('https');
 
 const baseUrl = 'https://www.easports.com/iframe/nhl14proclubs/api/platforms';
 var platformSelector = {
-    "PS4":1,
-    "XboxOne":2
+    "PS4":"ps4",
+    "Xbox":"xboxone"
 };
 
 
-function getClubId(clubName, platform) {
-    https.get(baseUrl+'/xboxone/clubsComplete/'+clubName, function(error,response,body) {
-        if(error) {
-                console.log(error);
-        } else {
-                console.log(response);
-        }
+
+
+function getDetailedClubStats(clubName, platform, callback) {
+    this.getClubId(clubName, platformSelector.Xbox, function(res) {
+        var url = baseUrl+'/'+platform+'/clubs/'+res+'/stats';
+        https.get(url, function(res){
+            res.on("data", function(chunk) {
+                var myData = JSON.parse(chunk);
+                callback(myData);
+            });
+        });
     });
 }
 
+function getClubStats(clubName, platform, callback) {
+    var url = baseUrl+'/'+platform+'/clubsComplete/'+clubName;
+    https.get(url, function(res) {
+        res.on("data", function(chunk) {
+            var myData = JSON.parse(chunk);
+            callback(myData);
+        });
+    }).on('error', function(e){
+        console.log('error');
+    });
+}
 
+function getClubId(clubName, platform, callback) {
+    var url = baseUrl+'/xboxone/clubsComplete/'+clubName;
+    https.get(url, function(res) {
+        res.on("data", function(chunk) {
+            var myData = JSON.parse(chunk);
+            callback(Object.keys(myData.raw)[0]);
+        });
+    }).on('error', function(e){
+        console.log('error');
+    });
+}
 
-
-console.log(getClubId('megaforce','xboxone'));
+getClubId('megaforce',platformSelector.Xbox, function(res){
+    console.log(res);
+});

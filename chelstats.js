e2602fb608ca916992ac73876c1eac2c80f55ce7
ravibhagw <38326056+ravibhagw@users.noreply.github.com>
2018-05-07 17:47:18 -0400

    basic working version!!

--- chelstats.js
+++ chelstats.js
@@ -8,33 +8,6 @@ var platformSelector = {
     "Xbox":"xboxone"
 };
 
-
-
-
-function getDetailedClubStats(clubName, platform, callback) {
-    this.getClubId(clubName, platformSelector.Xbox, function(res) {
-        var url = baseUrl+'/'+platform+'/clubs/'+res+'/stats';
-        https.get(url, function(res){
-            res.on("data", function(chunk) {
-                var myData = JSON.parse(chunk);
-                callback(myData);
-            });
-        });
-    });
-}
-
-function getClubStats(clubName, platform, callback) {
-    var url = baseUrl+'/'+platform+'/clubsComplete/'+clubName;
-    https.get(url, function(res) {
-        res.on("data", function(chunk) {
-            var myData = JSON.parse(chunk);
-            callback(myData);
-        });
-    }).on('error', function(e){
-        console.log('error');
-    });
-}
-
 function getClubId(clubName, platform, callback) {
     var url = baseUrl+'/xboxone/clubsComplete/'+clubName;
     https.get(url, function(res) {
@@ -47,6 +20,34 @@ function getClubId(clubName, platform, callback) {
     });
 }
 
-getClubId('megaforce',platformSelector.Xbox, function(res){
+
+
+function getDetailedClubStats(clubName, platform, callback) {
+    getClubId(clubName, platformSelector.Xbox, function(clubId) {
+        var url = baseUrl+'/'+platform+'/clubs/'+clubId+'/stats';
+        https.get(url, function(res){
+            res.on("data", function(chunk) {
+                var myData = JSON.parse(chunk);
+                callback(myData.raw[clubId]);
+            });
+        });
+    });
+}
+
+function getClubStats(clubName, platform, callback) {
+    var url = baseUrl+'/'+platform+'/clubsComplete/'+clubName;
+    https.get(url, function(res) {
+        res.on("data", function(chunk) {
+            var myData = JSON.parse(chunk);
+            callback(myData.raw);
+        });
+    }).on('error', function(e){
+        console.log('error');
+    });
+}
+
+
+getDetailedClubStats('megaforce',platformSelector.Xbox, function(res){
     console.log(res);
 });
+

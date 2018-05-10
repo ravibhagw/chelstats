7c526a15d6f5418f59917b5e9673b705def9ad35
ravibhagw <38326056+ravibhagw@users.noreply.github.com>
2018-05-10 09:54:11 -0400

    basic working version

--- chelstats.js
+++ chelstats.js
@@ -1,4 +1,5 @@
-// Chelstats v1.0
+// Chelstats v0.1
+// Using Node.js, this library can retrieve EASHL club and player stats
 var https = require('https');
 
 
@@ -47,7 +48,41 @@ function getClubStats(clubName, platform, callback) {
 }
 
 
-getDetailedClubStats('megaforce',platformSelector.Xbox, function(res){
-    console.log(res);
-});
+function getPlayersStats(clubName, platform, callback) {
+    getClubId(clubName, platformSelector.Xbox, function(clubId) {
+        var url = baseUrl+'/'+platform+'/clubs/'+clubId+'/members';
+        https.get(url, function(res){
+            res.on("data", function(chunk) {
+                var myData = JSON.parse(chunk);
+                
+                var players = [];
+                
+                var playersFormatted = [];
+
+                Object.keys(myData.raw[0]).forEach(function(itemKey){
+                    var item = myData.raw[0][itemKey];
+                    var blazeId = item.blazeId;
+                    players.push({"gamertag":item.name, "blazeId":blazeId});
+
+                });
+                
+                Promise.all(players.map((item) => {
+                    var requestUrl = baseUrl + '/' + platform + '/members/' + item.blazeId + '/stats';
+                    return new Promise((resolve) => {
+                        https.get(requestUrl, function(res) {
+                            res.on('data', function(chunk) {
+                              const playerData = JSON.parse(chunk);
+                              resolve({
+                                "gamertag": item.gamertag,
+                                "data": playerData.raw[Object.keys(playerData.raw)[0]]
+                              });
+                            });
+                        });
+                    });
+                }))
+                .then(callback);          
+            });
+        });
+    });
+}
 
